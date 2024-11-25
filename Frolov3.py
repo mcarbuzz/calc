e55aa1223a48218b2080988ed2e19f16aa407342
Frolov2 <frolov@maksim.ru>
2024-11-25 19:53:12 +0300

    Добавлено еще два файла

---
+++ Frolov3.py
@@ -0,0 +1,14 @@
+import math
+
+def modulo(a, b):
+    return a % b
+
+def power(a, b):
+    return a ** b
+
+def sine(a):
+    return math.sin(math.radians(a))
+
+def cosine(a):
+    return math.cos(math.radians(a))
+
